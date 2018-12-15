c77083bd565083d10160aea003079b7aa52d9525
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2018-12-15 14:33:21 -0500

    created links to frames and audio for linux

---
+++ src/make_links_unix.sh
@@ -0,0 +1,5 @@
+ln -s /home/michael/Dropbox/Harvard-AM-205-Basketball/Video/06_Frames_PNG /home/michael/IACS/AM-205-Basketball/frames
+
+ln -s /home/michael/Dropbox/Harvard-AM-205-Basketball/Video/05_Audio_WAV /home/michael/IACS/AM-205-Basketball/audio
+
+
