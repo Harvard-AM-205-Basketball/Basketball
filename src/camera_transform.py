2a4bdae76f8e60e475456e3f12d31c5a5834f792
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2018-12-17 17:16:21 -0500

    corrected make_transform in camera_transform.py

--- src/camera_transform.py
+++ src/camera_transform.py
@@ -79,15 +79,18 @@ def make_transform(cam_pos: np.ndarray, cam_point: np.ndarray, zoom: float):
     # The transform function
     def transform(object_pos):
         """The transform for this camera"""
+        # Reshape the object if necessary so it's an Nx3 matrix
+        object_pos = object_pos.reshape((-1,3))
         # Compute the position of this object relative to the camera, in the world frame (i.e. coordinates UVW)
         object_UVW = object_pos - cam_pos
         # Apply the rotation Rt to the local object coordinates 
         # to get the position of this object in the camera frame (i.e. coordinates XYZ)
         object_XYZ = np.matmul(object_UVW, Rt)
         # Distance of this object in the Z-plane
-        object_Z: float = object_XYZ[2]
+        object_Z: float = object_XYZ[:,2]
         # Convert to 2D using focal length and zoom
-        object_xy = (focus_eff / object_Z) * object_XYZ[0:2]
+        scaler = np.reshape(focus_eff / object_Z, (-1, 1))
+        object_xy = scaler * object_XYZ[:,0:2]
         # This transform returns (x, y) coordinates of objects
         return object_xy
     
@@ -101,62 +104,7 @@ cam_pos = np.array([22,-1.5, 5.2])
 cam_point = np.array([0.0, 41.0, 8.0])
 
 # Transform for camera 3
-# transform = make_transform(cam_pos, cam_point, focus, 1.0)
+transform = make_transform(cam_pos, cam_point, 1.0)
 
-# Apply the transform to points in the key box
-
-
-zoom = 1.0
-# The effective focus is the manufacturing focus divided by the zoom
-focus_eff = focus / zoom
-
-# Camera z-axis in world coords is the difference between 
-# where the camera is pointing and where it is located
-# z = np.array([-22, 42.5, 2.8]) # 2 worked well
-z = cam_point - cam_pos
-
-# Camera x-axis in world coords
-# x=np.array([42.5,22,0])
-x = np.array([z[1], -z[0], 0.0])
-
-# APPLY TRANSFORMATIONS:
-# Normalize each vector and make a rotation matrix R
-x = x / np.linalg.norm(x)
-z = z / np.linalg.norm(z)
-
-# Get y-axis via cross product
-y = np.cross(x, z)
-# Build rotation matrix and its transpose
-R = np.vstack((x, y, z))
-Rt = np.transpose(R)
-
-# The transform function
-def transform(object_pos):
-    """The transform for this camera"""
-    # Compute the position of this object relative to the camera, in the world frame (i.e. coordinates UVW)
-    object_UVW = object_pos - cam_pos
-    # Apply the rotation Rt to the local object coordinates 
-    # to get the position of this object in the camera frame (i.e. coordinates XYZ)
-    object_XYZ = np.matmul(object_UVW, Rt)
-    # Distance of this object in the Z-plane
-    object_Z: float = object_XYZ[2]
-    # Convert to 2D using focal length and zoom
-    object_xy = (focus / object_Z) * object_XYZ[0:2]
-    # This transform returns (x, y) coordinates of objects
-    return object_xy
-
-
-
-#    # Subtract position of camera on tripod
-#    coords = coords - campos
-#    
-#    # Apply rotation matrix (easier to use R^T here)
-#    coords=np.matmul(coords, Rt)
-#    
-#    # Convert to 2D by using focal length
-#    coords[:,0]=f*coords[:,0]/coords[:,2] # Divide X by Z and multiply by f
-#    coords[:,1]=f*coords[:,1]/coords[:,2] # Divide Y by Z and multiply by f
-#    
-#    
-#    # Delete z-coordinate (not needed since pixel plane is 2D)
-#    coords = np.delete(coords, 2, 1)
+# Apply the transform for camera 3 to points in the key box
+key_box_xy = transform(key_box)
