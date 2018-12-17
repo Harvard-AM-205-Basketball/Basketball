27eddd2db466d08fe269a1c56fd02d8ab245b14c
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2018-12-17 08:46:58 -0500

    added link to synch_frames from repo to dropbox

--- src/make_links_linux.sh
+++ src/make_links_linux.sh
@@ -1,5 +1,4 @@
-ln -s /home/michael/Dropbox/Harvard-AM-205-Basketball/Video/06_Frames_PNG /home/michael/IACS/AM-205-Basketball/frames
-
 ln -s /home/michael/Dropbox/Harvard-AM-205-Basketball/Video/05_Audio_WAV /home/michael/IACS/AM-205-Basketball/audio
-
+ln -s /home/michael/Dropbox/Harvard-AM-205-Basketball/Video/06_Frames_PNG /home/michael/IACS/AM-205-Basketball/frames
+ln -s /home/michael/Dropbox/Harvard-AM-205-Basketball/Video/07_SynchFrames /home/michael/IACS/AM-205-Basketball/synch_frames
 
