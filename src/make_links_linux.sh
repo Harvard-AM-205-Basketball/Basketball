1682ee9f6cf1d3375519e20309f7024322af3161
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2018-12-19 17:44:50 -0500

    cleaned up symlinks

--- src/make_links_linux.sh
+++ src/make_links_linux.sh
@@ -1,4 +1,9 @@
 ln -s /home/michael/Dropbox/Harvard-AM-205-Basketball/Video/05_Audio_WAV /home/michael/IACS/AM-205-Basketball/audio
 ln -s /home/michael/Dropbox/Harvard-AM-205-Basketball/Video/06_Frames_PNG /home/michael/IACS/AM-205-Basketball/frames
 ln -s /home/michael/Dropbox/Harvard-AM-205-Basketball/Video/07_SyncFrames /home/michael/IACS/AM-205-Basketball/sync_frames
+ln -s /home/michael/Dropbox/Harvard-AM-205-Basketball/Video/08_Sync_MPG /home/michael/IACS/AM-205-Basketball/sync_video
 ln -s /home/michael/Dropbox/Harvard-AM-205-Basketball/Video/09_Foreground /home/michael/IACS/AM-205-Basketball/foreground
+ln -s /home/michael/Dropbox/Harvard-AM-205-Basketball/Video/10_xy_coordinates /home/michael/IACS/AM-205-Basketball/ball_pixel
+ln -s /home/michael/Dropbox/Harvard-AM-205-Basketball/Video/11_BallFrames /home/michael/IACS/AM-205-Basketball/ball_frames
+ln -s /home/michael/Dropbox/Harvard-AM-205-Basketball/Video/12_Tableau /home/michael/IACS/AM-205-Basketball/tableau
+
